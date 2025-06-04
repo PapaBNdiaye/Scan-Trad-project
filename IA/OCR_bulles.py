6fa438dadccb4a55456ce9e36c7710473b3edd41
cjezierski <corentin.jezierski@gmail.com>
2025-06-04 16:06:58 +0200

    supression de newH et newW qui sont inutiles

--- IA/OCR_bulles.py
+++ IA/OCR_bulles.py
@@ -41,30 +41,17 @@ def yolo_to_pixel(cx_norm, cy_norm, w_norm, h_norm, img_width, img_height):
     return xmin, ymin, xmax, ymax
 
 
-def get_texts(path_image, boxes, newW, newH, padding=0):
+def get_texts(path_image, boxes, padding=0):
     # load the input image and grab the image dimensions
     image = cv2.imread(path_image)
     orig = image.copy()
-    (origH, origW) = image.shape[:2]
-    # set the new width and height and then determine the ratio in change
-    # for both the width and height
-    rW = origW / float(newW)
-    rH = origH / float(newH)
-    # resize the image and grab the new image dimensions
-    image = cv2.resize(image, (newW, newH))
     (H, W) = image.shape[:2]
 
     # initialize the list of results
     results = []
     # loop over the bounding boxes
     for (centerX, centerY, width, heigth) in boxes:
-        startX, startY, endX, endY = yolo_to_pixel(centerX, centerY, width, heigth, newW, newH)
-        # scale the bounding box coordinates based on the respective
-        # ratios
-        startX = int(startX * rW)
-        startY = int(startY * rH)
-        endX = int(endX * rW)
-        endY = int(endY * rH)
+        startX, startY, endX, endY = yolo_to_pixel(centerX, centerY, width, heigth, W, H)
         # in order to obtain a better OCR of the text we can potentially
         # apply a bit of padding surrounding the bounding box -- here we
         # are computing the deltas in both the x and y directions
@@ -73,8 +60,8 @@ def get_texts(path_image, boxes, newW, newH, padding=0):
         # apply padding to each side of the bounding box, respectively
         startX = max(0, startX - dX)
         startY = max(0, startY - dY)
-        endX = min(origW, endX + (dX * 2))
-        endY = min(origH, endY + (dY * 2))
+        endX = min(H, endX + (dX * 2))
+        endY = min(W, endY + (dY * 2))
         # extract the actual padded ROI
         roi = orig[startY:endY, startX:endX]
 
@@ -99,7 +86,6 @@ def get_texts(path_image, boxes, newW, newH, padding=0):
 
 # variables pour l'exemple
 path_image = "../data/train_version_YOLO_V8/images/DS_2_jpg.rf.0070a2c64c6c6389ab8bbcc4c8d0287b.jpg"
-(newW, newH) = (640, 640)
 padding = 0
 boxes = [
     [0.44609375, 0.11484375, 0.2265625, 0.12734375],
@@ -107,4 +93,4 @@ boxes = [
     [0.39140625, 0.303125, 0.1828125, 0.10546875]
 ]
 
-# print(get_texts(path_image,boxes, newW, newH))
+print(get_texts(path_image,boxes))
