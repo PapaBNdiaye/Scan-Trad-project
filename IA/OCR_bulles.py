222d5791950099c2459bc7014f7fb950b750955a
cjezierski <corentin.jezierski@gmail.com>
2025-06-05 15:42:41 +0200

    ocr wip

--- IA/OCR_bulles.py
+++ IA/OCR_bulles.py
@@ -6,15 +6,18 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import easyocr
+from PIL import Image
 import cv2
 import pytesseract
 
-from transformers import pipeline
+from transformers import pipeline, TrOCRProcessor, VisionEncoderDecoderModel
+
+# Charger modèle
 
 corrector = pipeline("text2text-generation", model="prithivida/grammar_error_correcter_v1")
 
 def correct_text(text):
-    result = corrector(text, max_new_tokens=100)[0]["generated_text"]
+    result = corrector(text, max_new_tokens=100, num_return_sequences=1)[0]["generated_text"]
     return result
 
 
@@ -59,13 +62,14 @@ def get_texts(path_image, boxes, padding=0):
     orig = image.copy()
 
     #pré-traitement
-    denoised = cv2.fastNlMeansDenoising(image, h=30)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-    processed = cv2.dilate(denoised, kernel, iterations=1)
-    gray = cv2.cvtColor(processed, cv2.COLOR_BGR2GRAY)  # conversion en niveaux de gris
+
+    # Agrandissement pour meilleure lisibilité
+    image = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # conversion en niveaux de gris
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]  # seuil de niveaux de gris
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    sharpened = cv2.filter2D(thresh, -1, kernel)  # améliorer la netteté
+    sharpened = cv2.filter2D(image, -1, kernel)  # améliorer la netteté
 
     (H, W) = image.shape[:2]
 
@@ -86,6 +90,7 @@ def get_texts(path_image, boxes, padding=0):
         endY = min(W, endY + (dY * 2))
         # extract the actual padded ROI
         roi = sharpened[startY:endY, startX:endX]
+        cv2.imwrite(str(index)+"test_roi.jpg", roi)
 
         # in order to apply Tesseract v4 to OCR text we must supply
         # (1) a language, (2) an OEM flag of 1, indicating that the we
@@ -98,60 +103,13 @@ def get_texts(path_image, boxes, padding=0):
         # of results
         text_cleaned = "".join([c if ord(c) < 128 else "" for c in text]).strip()
         text_corrected= correct_text(text_cleaned)
-        boxes[index].append(text_corrected)
+        text_final = text_corrected.split("|")[0]
+        print(text_final)
+        boxes[index].append(text_final)
     return boxes
 
 
-def add_texts_in_dataset(path_image, boxes, padding=0):
-    # load the input image and grab the image dimensions
-    image = cv2.imread(path_image)
-    orig = image.copy()
 
-    #pré-traitement
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # conversion en niveaux de gris
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]  # seuil de niveaux de gris
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    sharpened = cv2.filter2D(thresh, -1, kernel)  # améliorer la netteté
-
-    (H, W) = image.shape[:2]
-
-    # initialize the list of results
-    results = []
-    # loop over the bounding boxes
-    for index, (centerX, centerY, width, heigth) in enumerate(boxes):
-        startX, startY, endX, endY = yolo_to_pixel(centerX, centerY, width, heigth, W, H)
-        # in order to obtain a better OCR of the text we can potentially
-        # apply a bit of padding surrounding the bounding box -- here we
-        # are computing the deltas in both the x and y directions
-        dX = int((endX - startX) * padding)
-        dY = int((endY - startY) * padding)
-        # apply padding to each side of the bounding box, respectively
-        startX = max(0, startX - dX)
-        startY = max(0, startY - dY)
-        endX = min(H, endX + (dX * 2))
-        endY = min(W, endY + (dY * 2))
-        # extract the actual padded ROI
-        roi = sharpened[startY:endY, startX:endX]
-
-        # in order to apply Tesseract v4 to OCR text we must supply
-        # (1) a language, (2) an OEM flag of 1, indicating that the we
-        # wish to use the LSTM neural net model for OCR, and finally
-        # (3) an OEM value, in this case, 7 which implies that we are
-        # treating the ROI as a single line of text
-        config = ("-l eng --psm 6")
-        text = " ".join(reader.readtext(roi, detail=0))
-        print(text)
-        text_cleaned = "".join([c if ord(c) < 128 else "" for c in text]).strip()
-        text_corrected = correct_text(text_cleaned)
-        print(text_corrected)
-        # text = pytesseract.image_to_string(roi, config=config)
-        # # add the bounding box coordinates and OCR'd text to the list
-        # # of results
-
-        # with open("dataset.txt", "a", encoding="utf-8") as f:
-        #     f.write(text_corrected+"\n")
-
-    return boxes
 
 
 # variables pour l'exemple
@@ -169,4 +127,6 @@ boxes = [
 
 
 reader = easyocr.Reader(['en'], gpu=False)
-add_texts_in_dataset(path_image, boxes)
+
+
+get_texts(path_image, boxes)
