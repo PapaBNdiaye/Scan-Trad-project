7b463d3f8be5917a27e352413a749b4389ff9a51
cjezierski <corentin.jezierski@gmail.com>
2025-06-05 11:56:10 +0200

    ajout de la fonction pour get les textes dans un dataset

--- IA/OCR_bulles.py
+++ IA/OCR_bulles.py
@@ -98,6 +98,54 @@ def get_texts(path_image, boxes, padding=0):
     return boxes
 
 
+def add_texts_in_dataset(path_image, boxes, padding=0):
+    # load the input image and grab the image dimensions
+    image = cv2.imread(path_image)
+    orig = image.copy()
+
+    #pré-traitement
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # conversion en niveaux de gris
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]  # seuil de niveaux de gris
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    sharpened = cv2.filter2D(thresh, -1, kernel)  # améliorer la netteté
+
+    (H, W) = image.shape[:2]
+
+    # initialize the list of results
+    results = []
+    # loop over the bounding boxes
+    for index, (centerX, centerY, width, heigth) in enumerate(boxes):
+        startX, startY, endX, endY = yolo_to_pixel(centerX, centerY, width, heigth, W, H)
+        # in order to obtain a better OCR of the text we can potentially
+        # apply a bit of padding surrounding the bounding box -- here we
+        # are computing the deltas in both the x and y directions
+        dX = int((endX - startX) * padding)
+        dY = int((endY - startY) * padding)
+        # apply padding to each side of the bounding box, respectively
+        startX = max(0, startX - dX)
+        startY = max(0, startY - dY)
+        endX = min(H, endX + (dX * 2))
+        endY = min(W, endY + (dY * 2))
+        # extract the actual padded ROI
+        roi = sharpened[startY:endY, startX:endX]
+
+        # in order to apply Tesseract v4 to OCR text we must supply
+        # (1) a language, (2) an OEM flag of 1, indicating that the we
+        # wish to use the LSTM neural net model for OCR, and finally
+        # (3) an OEM value, in this case, 7 which implies that we are
+        # treating the ROI as a single line of text
+        config = ("-l eng --psm 6")
+        text = pytesseract.image_to_string(roi, config=config)
+        # add the bounding box coordinates and OCR'd text to the list
+        # of results
+        text_cleaned = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+        text_corrected= correct_text(text_cleaned)
+        with open("dataset.txt", "a", encoding="utf-8") as f:
+            f.write(text_corrected+"\n")
+
+    return boxes
+
+
 # variables pour l'exemple
 path_image = "../data/train_version_YOLO_V8/images/DS_11_jpg.rf.7dea8b7e0a5f3623f8c68550a201d943.jpg"
 padding = 0
@@ -111,4 +159,4 @@ boxes = [
     [0.2609375, 0.4546875, 0.103125, 0.090625]
 ]
 
-# print(get_texts(path_image, boxes))
+add_texts_in_dataset(path_image, boxes)
